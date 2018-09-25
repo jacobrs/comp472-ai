fe734b830039e802df8b4136523b5907431f1ade
Jacob Gagne <jacob_gagne@hotmail.com>
2018-09-25 16:25:40 -0400

    added input parsing for custom puzzles

--- main.go
+++ main.go
@@ -1,7 +1,25 @@
 package main
 
+import (
+	"os"
+	"strconv"
+	"strings"
+)
+
 func main() {
-	getBoard(3, 4).print()
+	if len(os.Args) > 1 {
+		strList := (strings.Split(os.Args[1], ","))
+		numList := []int{}
+		for _, i := range strList {
+			num, e := strconv.Atoi(i)
+			if e == nil {
+				numList = append(numList, num)
+			}
+		}
+		createBoard(numList).print()
+	} else {
+		getBoard(3, 4).print()
+	}
 }
 
 func dfs() {}
