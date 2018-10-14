101d60ce75cf9ea29d8238c698da82997571440b
Jacob Gagne <jacob_gagne@hotmail.com>
2018-10-14 19:01:59 -0400

    fixed asearch

--- assignment1/asearch.go
+++ assignment1/asearch.go
@@ -1,5 +1,12 @@
 package main
 
 func (g GameState) aSearch(fn func(board) float64) []string {
-	return g.genericSearch(func(g GameState) int { return int(g.parent.cost + 1) }, fn)
+	return g.genericSearch(aSearchCost, fn)
+}
+
+func aSearchCost(g GameState) int {
+	if g.parent != nil {
+		return int(g.parent.cost + 1)
+	}
+	return 0
 }
