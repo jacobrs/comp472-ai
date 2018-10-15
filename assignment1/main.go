736233088a01f1c1be1e0c47a488bb1f3e5f217b
benrs <benjaminbarault@gmail.com>
2018-10-14 22:18:17 -0400

    Added experimenting code

--- assignment1/main.go
+++ assignment1/main.go
@@ -9,12 +9,29 @@ import (
 )
 
 func main() {
+	programMode := "assignment"
 
+	if len(os.Args) > 1 {
+		programMode = os.Args[1]
+	}
+
+	if programMode == "assignment" {
+		assignmentMode()
+	} else {
+		experimentMode()
+	}
+}
+
+func assignmentMode() {
 	searchAlgorithm := "dfs"
 	var b board
 
-	if len(os.Args) > 1 {
-		strList := (strings.Split(os.Args[1], " "))
+	if len(os.Args) > 2 {
+		searchAlgorithm = os.Args[2]
+	}
+
+	if len(os.Args) > 3 {
+		strList := (strings.Split(os.Args[3], " "))
 		numList := []int{}
 		for _, i := range strList {
 			num, e := strconv.Atoi(i)
@@ -23,10 +40,6 @@ func main() {
 			}
 		}
 		b = createBoard(numList)
-
-		if len(os.Args) > 2 {
-			searchAlgorithm = os.Args[2]
-		}
 	} else {
 		b = getBoard(3, 4)
 	}
@@ -49,6 +62,54 @@ func main() {
 		game.gameStats = &GameStatistics{0, 0.0, 0, time.Time{}, time.Time{}, 0}
 		fmt.Println(game.aSearch(cartesianDistanceHeuristic))
 	}
+}
+
+func experimentMode() {
+	var b board
+	var numRuns int64 = 5
+	var e error
+
+	if len(os.Args) > 2 {
+		numRuns, e = strconv.ParseInt(os.Args[2], 10, 8)
+
+		if e != nil {
+			fmt.Println("Was expecting an integer for the number of runs")
+			os.Exit(1)
+		}
+	}
+
+	if len(os.Args) > 3 {
+		strList := (strings.Split(os.Args[3], " "))
+		numList := []int{}
+		for _, i := range strList {
+			num, e := strconv.Atoi(i)
+			if e == nil {
+				numList = append(numList, num)
+			}
+		}
+		b = createBoard(numList)
+		runExperiment(b, cartesianDistanceHeuristic)
+	} else {
+		for i := 0; i < int(numRuns); i++ {
+			b = getBoard(3, 4)
+
+			runExperiment(b, cartesianDistanceHeuristic)
+		}
+	}
+}
+
+func runExperiment(b board, heuristic func(board) float64) {
+	var gameBFS GameState
+	gameBFS.state = b
+	gameBFS.gameStats = &GameStatistics{0, 0.0, 0, time.Time{}, time.Time{}, 0}
+	fmt.Println(fmt.Sprintf("Running best first search on %s", b.key()))
+	fmt.Println(gameBFS.bestFirstSearch(heuristic))
+
+	var gameAStar GameState
+	gameAStar.state = b
+	gameAStar.gameStats = &GameStatistics{0, 0.0, 0, time.Time{}, time.Time{}, 0}
+	fmt.Println(fmt.Sprintf("Running A* search on %s", b.key()))
+	fmt.Println(gameAStar.aSearch(heuristic))
 }
 
 func prettyPrintPath(path []string) {
