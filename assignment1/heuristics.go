a370c57647c3c3eae191422f4bbf4352c5beeed3
Jacob Gagne <jacob_gagne@hotmail.com>
2018-10-14 23:07:08 -0400

    added modified manhattan heuristic

--- assignment1/heuristics.go
+++ assignment1/heuristics.go
@@ -19,3 +19,22 @@ func cartesianDistanceHeuristic(b board) float64 {
 	}
 	return cost
 }
+
+func modifiedManhattanDistanceHeuristic(b board) float64 {
+	cost := 0.0
+	for i, row := range b {
+		for j, num := range row {
+			if num != 0 {
+				properRow, properCol := (num-1)/4, (num-1)%4
+
+				rowDiff := Abs(properRow - i)
+				colDiff := Abs(properCol - j)
+
+				diff := Abs(rowDiff - colDiff)
+
+				cost += float64(diff + Min(rowDiff, colDiff))
+			}
+		}
+	}
+	return cost
+}
